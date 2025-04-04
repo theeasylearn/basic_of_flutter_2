d3af3341084ffba4d7c6e1a00b79417c44384f3f
theeasylearn <theeasylearn@gmail.com>
2025-04-04 13:44:26 +0530

    push 1

---
+++ lib/column_row_demo_2.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class Column_row_Demo_2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+          child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Expanded(
+                    flex: 1,
+                    child: Container(
+                      alignment: Alignment.topLeft,
+                      child: Text("IPhone 16 pro max"),
+                    )),
+                Expanded(
+                    flex: 5,
+                    child: Container(
+                        alignment: Alignment.topLeft,
+                        child: Text("we will display Image here"))),
+                Expanded(
+                    flex: 5,
+                    child: Text("we will display product description here")),
+                Expanded(
+                    flex: 1,
+                    child: Row(
+                      children: [
+                        Expanded(
+                            flex: 3,
+                            child: ElevatedButton(
+                                onPressed: () {}, child: Text("Add to cart"))),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Expanded(
+                            flex: 2,
+                            child: ElevatedButton(
+                                onPressed: () {}, child: Text("Add to wishlist")))
+                      ],
+                    ))
+              ])),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,54 @@
 import 'package:flutter/material.dart';
 import 'column_demo_1.dart';
 import 'column_row_demo.dart';
-
-class Column_row_Demo_2 extends StatelessWidget {
+class CardDemo1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-          child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-            Expanded(
-                flex: 1,
-                child: Container(
-                  alignment: Alignment.topLeft,
-                  child: Text("IPhone 16 pro max"),
-                )),
-            Expanded(
-                flex: 5,
-                child: Container(
-                    alignment: Alignment.topLeft,
-                    child: Text("we will display Image here"))),
-            Expanded(
-                flex: 5,
-                child: Text("we will display product description here")),
-            Expanded(
-                flex: 1,
-                child: Row(
-                  children: [
-                    Expanded(
-                        flex: 3,
-                        child: ElevatedButton(
-                            onPressed: () {}, child: Text("Add to cart"))),
-                    SizedBox(
-                      width: 10,
+        home: Material(
+            child: Center(
+              child: SizedBox(
+                height: 450,
+                child: Card(
+                    color: Colors.white,
+                    elevation: 06,
+                    child: Column(
+                      children: [
+                          ListTile(
+                            title: Text("IPhone 16 pro max"),
+                            subtitle: Text("Build for apple intelligence"),
+                            trailing: Icon(Icons.bookmark),
+                          ),
+                          Container(
+                            height: 200,
+                            width: double.infinity,
+                            child: Image.network(
+                                "https://picsum.photos/800/600",
+                                fit: BoxFit.cover,
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            child: Text("The iPhone 16 Pro Max redefines smartphone innovation with its sleek titanium design, "
+                                "ultra-responsive 6.9-inch Super Retina XDR display, and the powerful A18 Pro chip. "
+                                "Engineered for performance and efficiency, it delivers lightning-fast speed and advanced AI capabilities. "
+                                "The quad-camera system features a 48MP main lens, 10x optical zoom, and enhanced Night mode, "
+                                "offering pro-level photography. "),
+                          ),
+                          OverflowBar(
+                            alignment: MainAxisAlignment.end,
+                            children: [
+                                ElevatedButton(onPressed: () {}, child: Text("Add to cart")),
+                                SizedBox(width: 10,),
+                                ElevatedButton(onPressed: () {}, child: Text("Add to wishlist")),
+                            ],
+                          )
+                      ],
                     ),
-                    Expanded(
-                        flex: 2,
-                        child: ElevatedButton(
-                            onPressed: () {}, child: Text("Add to wishlist")))
-                  ],
-                ))
-          ])),
+                ),
+              ),
+            ),
+        ),
     );
   }
 }
@@ -50,5 +56,6 @@ class Column_row_Demo_2 extends StatelessWidget {
 void main() {
   // runApp(Column_Demo_1());
   // runApp(Column_Row_Demo_1());
-  runApp(Column_row_Demo_2());
+  // runApp(Column_row_Demo_2());
+  runApp(CardDemo1());
 }
