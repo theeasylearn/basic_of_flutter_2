72c9a7ef425115b995fa9fee617b401175037778
theeasylearn <theeasylearn@gmail.com>
2025-04-05 13:42:33 +0530

    push 1

---
+++ lib/card_demo.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class CardDemo1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: SizedBox(
+            height: 450,
+            child: Card(
+              color: Colors.white,
+              elevation: 06,
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text("IPhone 16 pro max"),
+                    subtitle: Text("Build for apple intelligence"),
+                    trailing: Icon(Icons.bookmark),
+                  ),
+                  Container(
+                    height: 200,
+                    width: double.infinity,
+                    child: Image.network(
+                      "https://picsum.photos/800/600",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: Text("The iPhone 16 Pro Max redefines smartphone innovation with its sleek titanium design, "
+                        "ultra-responsive 6.9-inch Super Retina XDR display, and the powerful A18 Pro chip. "
+                        "Engineered for performance and efficiency, it delivers lightning-fast speed and advanced AI capabilities. "
+                        "The quad-camera system features a 48MP main lens, 10x optical zoom, and enhanced Night mode, "
+                        "offering pro-level photography. "),
+                  ),
+                  OverflowBar(
+                    alignment: MainAxisAlignment.end,
+                    children: [
+                      ElevatedButton(onPressed: () {}, child: Text("Add to cart")),
+                      SizedBox(width: 10,),
+                      ElevatedButton(onPressed: () {}, child: Text("Add to wishlist")),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,81 @@
 import 'package:flutter/material.dart';
 import 'column_demo_1.dart';
 import 'column_row_demo.dart';
-class CardDemo1 extends StatelessWidget {
+import 'card_demo.dart';
+
+class SizedBoxDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Material(
-            child: Center(
-              child: SizedBox(
-                height: 450,
-                child: Card(
-                    color: Colors.white,
-                    elevation: 06,
-                    child: Column(
-                      children: [
-                          ListTile(
-                            title: Text("IPhone 16 pro max"),
-                            subtitle: Text("Build for apple intelligence"),
-                            trailing: Icon(Icons.bookmark),
-                          ),
-                          Container(
-                            height: 200,
-                            width: double.infinity,
-                            child: Image.network(
-                                "https://picsum.photos/800/600",
-                                fit: BoxFit.cover,
-                            ),
-                          ),
-                          Container(
-                            padding: EdgeInsets.all(10),
-                            child: Text("The iPhone 16 Pro Max redefines smartphone innovation with its sleek titanium design, "
-                                "ultra-responsive 6.9-inch Super Retina XDR display, and the powerful A18 Pro chip. "
-                                "Engineered for performance and efficiency, it delivers lightning-fast speed and advanced AI capabilities. "
-                                "The quad-camera system features a 48MP main lens, 10x optical zoom, and enhanced Night mode, "
-                                "offering pro-level photography. "),
-                          ),
-                          OverflowBar(
-                            alignment: MainAxisAlignment.end,
-                            children: [
-                                ElevatedButton(onPressed: () {}, child: Text("Add to cart")),
-                                SizedBox(width: 10,),
-                                ElevatedButton(onPressed: () {}, child: Text("Add to wishlist")),
-                            ],
-                          )
-                      ],
-                    ),
+        home: Center(
+            child: Container(
+                padding: EdgeInsets.all(10),
+                color: Color(0xAAFBFCFA),
+                alignment: Alignment.center,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                              SizedBox(
+                                  height: 200,
+                                  width:  200,
+                                  child: Card(
+                                      elevation: 10,
+                                      color: Color(0xaaFFFADA),
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: FittedBox(child: Text("Flutter")),
+                                      ),
+                                  ),
+                              ),
+                              SizedBox(
+                                  height: 200,
+                                  width:  200,
+                                  child: Card(
+                                      elevation: 10,
+                                      color: Color(0xaaFFFADA),
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: FittedBox(child: Text("MERN Stack")),
+                                      ),
+                                  ),
+                              ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                              SizedBox(
+                                  height: 200,
+                                  width:  200,
+                                  child: Card(
+                                      elevation: 10,
+                                      color: Color(0xaaFFFADA),
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: FittedBox(
+                                          child: Text("AI/ML"),
+                                        ),
+                                      ),
+                                  ),
+                              ),
+                              SizedBox(
+                                  height: 200,
+                                  width:  200,
+                                  child: Card(
+                                      elevation: 10,
+                                      color: Color(0xaaFFFADA),
+                                      child: Image.network("https://picsum.photos/150/150",
+                                      fit: BoxFit.cover,),
+                                  ),
+                              ),
+                          ],
+                        ),
+                    ],
                 ),
-              ),
             ),
         ),
     );
@@ -57,5 +86,6 @@ void main() {
   // runApp(Column_Demo_1());
   // runApp(Column_Row_Demo_1());
   // runApp(Column_row_Demo_2());
-  runApp(CardDemo1());
+  // runApp(CardDemo1());
+  runApp(SizedBoxDemo());
 }
